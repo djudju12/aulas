5ffcdf9597120f4596f4894ae0c23d6cb3f2c52f
djudju12 <jonathan.willian321@gmail.com>
2022-11-01 21:34:14 -0300

    aula01-11

---
+++ programacao-reso-prob/9.2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX 100
+
+void capitalizeString(char str[]);
+
+int main(){
+	char str[MAX];
+	
+	printf("Digite a string (ateh %d caracteres): ", MAX);
+	fgets(str, MAX, stdin);
+	capitalizeString(str);
+	
+	printf("%s", str);
+	return 0;
+}
+
+void capitalizeString(char str[])
+{
+	int count=0;
+	for(count=0;count<strlen(str);count++)
+	{
+		if(isalpha(str[count]))
+			str[count] = toupper(str[count]);
+	}
+	
+}
