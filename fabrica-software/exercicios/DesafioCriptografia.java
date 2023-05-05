24ba7970836d3854d8fa8afffc0db1cd4acd9804
djudju12 <jonathan.willian321@gmail.com>
2023-05-04 21:06:04 -0300

    asidjsa

--- fabrica-software/exercicios/DesafioCriptografia.java
+++ fabrica-software/exercicios/DesafioCriptografia.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 class DesafioCriptografia {
     public static void main(String[] args) {
-        String in = inputString();
-        // String in = new String("if man was meant to stay on the ground god would have given us roots");
+        // String in = inputString();
+        String in = new String("if man was meant to stay on the ground god would have given us roots");
         // L eh calculado antes ou depois de retirar os espacos?????
         in = in.replaceAll(" ", "");
         int L = in.length();
@@ -25,7 +25,7 @@ class DesafioCriptografia {
         // 3 groundgo => 24 25 | (uppderBound) * coluna + linha = 24 ... 25 ... 31
         // 4 dwouldha => 32 33 | (uppderBound) * coluna + linha = 32 ... 33 ... 39
         // 5 vegivenu => 40 34 | (uppderBound) * coluna + linha = 40 ... 34 ... 47
-        // 6 sroots => 48 49   | (uppderBound) * coluna + linha = 48 ... 49 ... 55 (não entra)
+        // 6 sroots => 48 49   | (uppderBound) * coluna + linha = 48 ... 49 ... 55 (nao entra)
 
         for (int linha = 0; linha < upperBound; linha++){    
             for (int coluna = 0; coluna < lowerBound; coluna++){
--- fabrica-software/exercicios/run.sh
+++ fabrica-software/exercicios/run.sh
@@ -1,5 +1,7 @@
 #!bin/bash/
+echo "expectativa \/" 
+echo "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau"
+echo "resultado \/ "
+
 javac DesafioCriptografia.java
 java DesafioCriptografia
-
-echo "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau"
