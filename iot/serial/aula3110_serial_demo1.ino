24742f482fdc344cce43f60d6334ecdcd79042ac
Jonathan Santos <39841260+djudju12@users.noreply.github.com>
2023-11-06 18:07:46 -0300

    Add files via upload

---
+++ iot/serial/aula3110_serial_demo1.ino
@@ -0,0 +1,38 @@
+// C++ code
+//
+#define LED    A0
+#define BUTTON A5
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  Serial.begin(9600);
+  while(!Serial)
+    ;
+}
+
+#define ON  7
+#define OFF 0
+
+void loop()
+{
+  int cmd = 0;
+  
+  if(Serial.available() > 0) {
+    cmd = Serial.read();
+    if (cmd == ON) {
+      digitalWrite(LED, HIGH);
+    } else {
+      digitalWrite(LED, LOW);
+    }
+  }
+	
+  if (digitalRead(BUTTON) == HIGH) {
+  	Serial.write(ON);
+  } else {
+    Serial.write(OFF);
+  }
+  
+  delay(250);
+}
