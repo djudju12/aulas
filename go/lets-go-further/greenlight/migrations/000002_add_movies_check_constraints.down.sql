83b4c652ea44e6841c28faa11f05806928a4ab58
Jonathan Santos <jonathan.willian321@gmail.com>
2023-11-08 15:31:29 -0300

    db connection lets go further

---
+++ go/lets-go-further/greenlight/migrations/000002_add_movies_check_constraints.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE moves DROP CONSTRAINT IF EXISTS movies_runtime_check;
+
+ALTER TABLE moves DROP CONSTRAINT IF EXISTS movies_year_check;
+
+ALTER TABLE moves DROP CONSTRAINT IF EXISTS genres_length_check;
