8886150f2a68e4f7ddba2279fef8e8ef25d83a9f
Jonathan Santos <jonathan.willian321@gmail.com>
2023-11-14 13:05:09 -0300

    lets go further: makefile

--- go/lets-go-further/greenlight/cmd/api/middleware.go
+++ go/lets-go-further/greenlight/cmd/api/middleware.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"expvar"
 	"fmt"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -14,6 +13,7 @@ import (
 	"github.com/djudju12/greenlight/internal/data"
 	"github.com/djudju12/greenlight/internal/validator"
 	"github.com/felixge/httpsnoop"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -82,11 +82,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enable {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
