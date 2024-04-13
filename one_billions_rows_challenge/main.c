9c1df42053ae3836bcf17d7c76092ac2ddff5dbc
Jonathan Santos <jonathan.willian321@gmail.com>
2024-04-13 19:48:56 -0300

    Update MAX_STATION_CNT and refactor code

--- one_billions_rows_challenge/main.c
+++ one_billions_rows_challenge/main.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <assert.h>
-#include <unistd.h>
 #include <time.h>
 
 #define STB_DS_IMPLEMENTATION
 #include "stb_ds.h"
 
-#define MAX_STATION_CNT 41344
+#define MAX_STATION_CNT 50000
 #define MAX_STATION_NAME_LEN 256
 
 typedef struct {
+    char *key;
     double min;
     double total;
     double max;
     int total_entries;
 } Station_Data;
-
-typedef struct {
-    char *key;
-    Station_Data value;
-} Station_Table;
 
 #define FMT_STATION "%s=(min(%.2lf), total(%.2lf), max(%.2lf), entries(%d))"
 #define ARGS_STATION(key, s) (key), (s).min, (s).total, (s).max, (s).total_entries
@@ -68,8 +61,7 @@ int main(void) {
     // const char *file_path = "weather_stations.csv";
 
     FILE *stream = fopen(file_path, "r");
-    Station_Table *stations_table;
-    stations_table = sh_new_strdup(stations_table);
+    Station_Data *stations_table = NULL;
 
     char station_name[MAX_STATION_NAME_LEN];
     char temperature[MAX_STATION_NAME_LEN];
@@ -81,13 +73,13 @@ int main(void) {
             printf("processed %ld rows in %f seconds\n", row, (double) (((double)clock()) - start) / CLOCKS_PER_SEC);
         }
 
-        Station_Data station = shget(stations_table, station_name);
+        Station_Data station = shgets(stations_table, station_name);
 
         if (station.total_entries == 0) {
             station.max = -99.00;
             station.min = +99.00;
             station.total = 0.00;
-            strcpy(keys[shlen(stations_table)], station_name);
+            station.key = strcpy(keys[shlen(stations_table)], station_name);
         }
 
         station.total_entries++;
@@ -103,18 +95,21 @@ int main(void) {
             station.max = v;
         }
 
-        shput(stations_table, station_name, station);
+        shputs(stations_table, station);
     }
 
+
     long table_len = shlen(stations_table);
+
     qsort(keys, table_len, sizeof(keys[0]), comparator);
+
     FILE *result = fopen("result.txt", "w");
     fprintf(result, "{");
 
     for (int i = 0; i < table_len; i++) {
-        Station_Data station = shget(stations_table, keys[i]);
-        double mean = station.total / station.total_entries;
+        Station_Data station = stations_table[i];
 
+        double mean = station.total / station.total_entries;
         fprintf(result, "%s=%.2lf/%.2lf/%.2lf", keys[i], station.min, mean, station.max);
         if (i < table_len-1) {
             fprintf(result, ", ");
