14b931affe37d012145a639c1a74efb1777174fd
Jonathan Santos <jwsantos@kopp.com.br>
2024-04-16 13:13:17 -0300

    add hash table impl

--- one_billions_rows_challenge/main.c
+++ one_billions_rows_challenge/main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
-
-#define STB_DS_IMPLEMENTATION
-#include "stb_ds.h"
+#include <stdlib.h>
 
 #define MAX_STATION_CNT 10000
 #define MAX_STATION_NAME_LEN 256
 
 typedef struct {
-    char *key;
+    char key[MAX_STATION_NAME_LEN];
     double min;
     double total;
     double max;
@@ -18,13 +16,6 @@ typedef struct {
 
 #define FMT_STATION "%s=%.2lf/%.2lf/%2.lf"
 #define ARGS_STATION(s) (s).key, (s).min, ((s).total / (s).total_entries), (s).max
-
-char keys[MAX_STATION_CNT][MAX_STATION_NAME_LEN];
-int comparator(const void *a, const void *b) {
-    const char (*str1)[MAX_STATION_NAME_LEN] = a;
-    const char (*str2)[MAX_STATION_NAME_LEN] = b;
-    return strcmp(*str1, *str2);
-}
 
 int mgetline(char *station, char *temperature, int size, FILE *stream) {
     char c;
@@ -56,64 +47,100 @@ int mgetline(char *station, char *temperature, int size, FILE *stream) {
     return 0;
 }
 
-int main(void) {
-    // const char *file_path = "/home/kopp/fontes/pessoal/1brc/measurements-10_000.txt";
-    const char *file_path = "/home/kopp/fontes/pessoal/1brc/measurements-1_000_000_000.txt";
+#define HASHMAP_CAPACITY 16384
+#define HASHMAP_INDEX(h) (h & (HASHMAP_CAPACITY - 1))
+typedef struct {
+    Station_Data entries[MAX_STATION_CNT];
+    unsigned int table[HASHMAP_CAPACITY];
+    unsigned int len;
+} Hash_Map;
 
-    FILE *stream = fopen(file_path, "r");
-    Station_Data *stations_table = NULL;
-    sh_new_arena(stations_table);
-
-    char station_name[MAX_STATION_NAME_LEN];
-    char temperature[MAX_STATION_NAME_LEN];
-    double v;
-    long row = 0;
-    clock_t start = clock();
-    while (mgetline(station_name, temperature, 1024, stream) != EOF)  {
-        // if (++row % 50000000 == 0) {
-        //     printf("processed %ld rows in %f seconds\n", row, (double) (((double)clock()) - start) / CLOCKS_PER_SEC);
-        // }
-
-        Station_Data *station = shgetp_null(stations_table, station_name);
-
-        if (station == NULL) {
-            strcpy(keys[shlen(stations_table)], station_name);
-            Station_Data s = {
-                .max = -99.00,
-                .min = +99.00,
-                .total = 0.00,
-                .key = station_name
-            };
-
-            shputs(stations_table, s);
-            station = shgetp_null(stations_table, station_name);
-            assert(station != NULL);
-        }
-
-        station->total_entries++;
-
-        sscanf(temperature, "%lf", &v);
-        station->total = station->total + v;
-
-        if (v < station->min) {
-            station->min = v;
-        }
-
-        if (v > station->max) {
-            station->max = v;
-        }
+#include <string.h>
+unsigned int *hm_get(Hash_Map *map, const char *key) {
+    unsigned int len = 0;
+    unsigned int h = 0;
+    for (;key[len] != '\0'; len++) {
+        h = (31*h) + (unsigned char)key[len];
     }
 
-    long table_len = shlen(stations_table);
+    unsigned int *c = &map->table[HASHMAP_INDEX(h)];
+    while (*c > 0 && memcmp(map->entries[*c].key, key, len) != 0) {
+        h++;
+        c = &map->table[HASHMAP_INDEX(h)];
+    }
 
-    qsort(keys, table_len, sizeof(keys[0]), comparator);
+    return c;
+}
+
+#define min(a, b) (a) < (b) ? (a) : (b)
+#define max(a, b) (a) > (b) ? (a) : (b)
+
+void hm_put(Hash_Map *map, const char *key, double temperature) {
+    unsigned int len = 0;
+    unsigned int h = 0;
+    for (;key[len] != '\0'; len++) {
+        h = (31*h) + (unsigned char)key[len];
+    }
+
+    unsigned int *c = &map->table[HASHMAP_INDEX(h)];
+    while (*c > 0 && memcmp(map->entries[*c].key, key, len) != 0) {
+        h++;
+        c = &map->table[HASHMAP_INDEX(h)];
+    }
+
+    if (*c == 0) {
+        *c = map->len;
+        memcpy(map->entries[*c].key, key, len);
+        map->len++;
+        map->entries[*c].max = temperature;
+        map->entries[*c].min = temperature;
+        map->entries[*c].total = temperature;
+        map->entries[*c].total_entries = 1;
+    } else {
+        map->entries[*c].max = max(temperature, map->entries[*c].max);
+        map->entries[*c].min = min(temperature, map->entries[*c].min);
+        map->entries[*c].total += temperature;
+        map->entries[*c].total_entries++;
+    }
+}
+
+int comparator(const void *a, const void *b) {
+    return strcmp(((Station_Data *)a)->key, ((Station_Data *)b)->key);
+}
+
+int main(int argc, char **argv) {
+    clock_t start = clock();
+
+    char *file_path = "/home/kopp/fontes/pessoal/1brc/measurements10k.txt";
+    if (argc > 1) file_path = argv[1];
+
+    FILE *stream = fopen(file_path, "r");
+
+    Hash_Map *map = malloc(sizeof(Hash_Map));
+    memset(map->table, 0, HASHMAP_CAPACITY * sizeof(*map->table));
+    memset(map->entries, 0, MAX_STATION_CNT * sizeof(*map->entries));
+
+    long row = 0;
+    double v;
+
+    char station_name[MAX_STATION_NAME_LEN], temperature[MAX_STATION_NAME_LEN];
+    while (mgetline(station_name, temperature, 1024, stream) != EOF)  {
+        if (++row % 50000000 == 0) {
+            printf("processed %ld rows in %f seconds\n", row, (double) (((double)clock()) - start) / CLOCKS_PER_SEC);
+        }
+
+        sscanf(temperature, "%lf", &v);
+        hm_put(map, station_name, v);
+    }
+
+    qsort(map->entries, map->len, sizeof(*map->entries), comparator);
 
     FILE *result = fopen("out/result.txt", "w");
     fprintf(result, "{");
-    for (int i = 0; i < table_len; i++) {
-        Station_Data station = shgets(stations_table, keys[i]);
+    for (int i = 0; i < map->len; i++) {
+        Station_Data station = map->entries[i];
         fprintf(result, FMT_STATION, ARGS_STATION(station));
-        if (i < table_len-1) {
+        if (i < map->len-1) {
             fprintf(result, ", ");
         }
     }
